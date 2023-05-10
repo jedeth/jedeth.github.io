7d94ad6274cbaf9cb191642d43f45534eae2c5ad
jedeth <jedeth@gmail.com>
2023-05-10 11:43:34 +0200

    10/05

--- app.js
+++ app.js
@@ -3,14 +3,34 @@
 
     angular.module('LunchCheck', [])
 
-    .controller('LunchCheckController', function($scope){
-            $scope.repas = "";
-            $scope.nombreDePlats = calculatriceNombreDePlats($scope.repas);
+    .controller('LunchCheckController', function ($scope){
+        console.log('ici !!!')
+            $scope.repas = "fff,eee,rr,,r";
+            // var Plats = $scope.repas; 
+            $scope.alerte = calculNbPlats($scope.repas);
 
-        // function calculatriceNombreDeRepas(string){
-        //     var totalNombreDeRepas = split();
+            $scope.displayAlerte = function() {
+                var xxx = calculNbPlats ($scope.repas);
+                $scope.alerte = xxx;
+            };
             
-        // }
-            console.log (scope.repas);
+            function calculNbPlats (string) {
+                console.log('calculNbPlats')
+                var tableauDesPlats = [];
+
+                tableauDesPlats = string.split(",");
+                var longeurTableau = 0;
+                longeurTableau = tableauDesPlats.length;
+
+                if (longeurTableau < 4 )
+                {
+                    return "enjoy !"
+                } else {
+                    return "trop !"
+                }
+            };
+
+                
+});
 
 })();
